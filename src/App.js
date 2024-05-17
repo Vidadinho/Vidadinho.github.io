e7d7fc2dbff665aa3d4ee6f29b2dccfa904160b9
vida <vidasamuel13@gmail.com>
2024-05-17 17:42:34 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Contact, Home, Login, Register, Reset } from "./pages";
+import { Footer, Header } from "./components";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <ToastContainer />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
